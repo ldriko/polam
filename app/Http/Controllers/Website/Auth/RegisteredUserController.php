81d760fca1422e286fa6d103f9e20e0ae274b77a
Anwarul Fattach <emosekolah@gmail.com>
2023-07-27 14:32:31 +0700

    tambahkan validasi email upn

--- app/Http/Controllers/Website/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Website/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class, 'ends_with:@student.upnjatim.ac.id'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 

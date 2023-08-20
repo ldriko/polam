4c537066b32d59b1a2d8d7953a8ecd6fa2ebcde3
Anwarul Fattach <emosekolah@gmail.com>
2023-08-20 19:44:28 +0700

    tampilan pkl sementara

--- resources/views/website/surat-pengantar/pkl/index.blade.php
+++ resources/views/website/surat-pengantar/pkl/index.blade.php
@@ -16,9 +16,77 @@
 
 <section class="inner-page">
   <div class="container">
-    <p>
-      Example inner page template
-    </p>
+    <header class="section-header">
+      <h2>Surat Pengantar PKL</h2>
+      <p>Riwayat Pengajuan</p>
+    </header>
+
+    <div class="d-flex align-items-center gap-2 mb-2">
+      <span>Unduh panduan pengajuan Surat Pengantar PKL</span>
+      <button class="btn btn-warning">Unduh</button>
+    </div>
+
+    <table class="table table-striped">
+      <thead class="table-dark text-center">
+        <tr>
+          <th>No.</th>
+          <th>Nama</th>
+          <th>Tanggal Pengajuan</th>
+          <th>Status Pengajuan</th>
+          <th>Periksa Dokumen</th>
+        </tr>
+      </thead>
+      <tbody class="text-center align-middle">
+        <tr>
+          <td>1.</td>
+          <td>Nama Mahasiswa</td>
+          <td>20 Juli 2023</td>
+          <td>Verifikasi Staff</td>
+          <td>
+            <button class="btn btn-primary">Buka</button>
+          </td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Nama Mahasiswa 2</td>
+          <td>22 Juli 2023</td>
+          <td>Verifikasi Wadek 1</td>
+          <td>
+            <button class="btn btn-primary">Buka</button>
+          </td>
+        </tr>
+        <tr>
+          <td>3.</td>
+          <td>Nama Mahasiswa 3</td>
+          <td>22 Juli 2023</td>
+          <td>Ditolak Staff</td>
+          <td>
+            <button class="btn btn-primary">Buka</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="mt-5">
+      <header class="section-header">
+        <h2>Surat Pengantar PKL</h2>
+        <p>Form Pengajuan</p>
+      </header>
+      <form action="#" method="post" class="php-email-form">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Form 1</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Form 2</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Form 2</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+      </form>
+    </div>
   </div>
 </section>
 @stop

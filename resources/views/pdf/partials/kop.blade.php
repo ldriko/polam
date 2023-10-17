c0713e0ae153dbbe6def23facb7574b30ad4e5e7
Anwarul Fattach <emosekolah@gmail.com>
2023-10-17 22:16:26 +0700

    kop surat sudah jadi

---
+++ resources/views/pdf/partials/kop.blade.php
@@ -0,0 +1,12 @@
+<div class="w-100 text-center py-10" style="border-bottom: 4px double black;">
+    <div class="text-center" style="display: inline-block; margin-right: 10px;">
+        <img class="logo vertical-align-middle" src="{{ asset('website/img/logo-upn.png') }}" alt="">
+    </div>
+    <div class="vertical-align-middle" style="display: inline-block;">
+        <span class="d-block text-center">KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN, RISET, DAN TEKNOLOGI</span>
+        <span class="d-block text-center bold">UNIVERSITAS PEMBANGUNAN NASIONAL "VETERAN" JAWA TIMUR</span>
+        <span class="d-block text-center bolder" style="font-size: 25px;">FAKULTAS ILMU KOMPUTER</span>
+        <span class="d-block text-center" style="font-size: 15px;">Jalan Raya Rungkut Madya, Gunung Anyar, Surabaya, 60294, Telp. 031-8706369</span>
+        <span class="d-block text-center" style="font-size: 13px;">Email: <a href="mailto:fasilkom@upnjatim.ac.id">fasilkom@upnjatim.ac.id</a>, Fax: 031-8706372, Laman: <a href="http://www.upnjatim.ac.id" target="_blank">http://www.upnjatim.ac.id</a></span>
+    </div>
+</div>
--- resources/views/pdf/surat-pengantar/pkl/index.blade.php
+++ resources/views/pdf/surat-pengantar/pkl/index.blade.php
@@ -1,12 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="width: 21cm; height: 29cm; margin: 0px;">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Surat Pengantar PKL</title>
+
+    <style>
+        .d-block {
+            display: block;
+        }
+        .text-center {
+            text-align: center;
+        }
+        .vertical-align-middle {
+            vertical-align: middle;
+        }
+        .bold {
+            font-weight: 700;
+        }
+        .bolder {
+            font-weight: 900;
+        }
+        .w-100 {
+            width: 100%;
+        }
+        .logo {
+            height: auto;
+            width: 100px;
+        }
+        .pb-10 {
+            padding-bottom: 10px;
+        }
+        .py-10 {
+            padding-bottom: 10px;
+            padding-top: 10px;
+        }
+        .px-10 {
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+        .px-50 {
+            padding-left: 50px;
+            padding-right: 50px;
+        }
+        .py-50 {
+            padding-top: 50px;
+            padding-bottom: 50px;
+        }
+    </style>
 </head>
-<body>
-    <h1>wkwkwk</h1>
+<body class="px-50 py-50" style="width: 21cm; height: 29cm; margin: 0px; position: relative; border: 1px solid red;">
+    @include('pdf.partials.kop')
 </body>
 </html>

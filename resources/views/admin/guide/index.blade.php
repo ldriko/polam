a7fa24ec225be1de6092fd2074f99bb39ab62360
Anwarul Fattach <emosekolah@gmail.com>
2025-01-06 04:23:55 +0700

    merapikan table

--- resources/views/admin/guide/index.blade.php
+++ resources/views/admin/guide/index.blade.php
@@ -24,7 +24,7 @@
           </div>
           <div class="card-body">
             <div class="table-responsive">
-              <table class="table table-striped table-md">
+              <table class="table table-striped">
                 <tr>
                   <th>#</th>
                   <th>Tipe</th>
--- resources/views/admin/surat-pengantar/skripsi/index.blade.php
+++ resources/views/admin/surat-pengantar/skripsi/index.blade.php
@@ -20,7 +20,7 @@
           </div>
           <div class="card-body">
             <div class="table-responsive">
-              <table class="table table-striped table-md">
+              <table class="table table-striped">
                 <tr>
                   <th>#</th>
                   <th>Nama Mahasiswa</th>

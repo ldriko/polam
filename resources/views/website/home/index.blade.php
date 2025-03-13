c5eb9f402e433117d45e58afdf800a0869ce3856
Anwarul Fattach <emosekolah@gmail.com>
2025-03-13 14:40:22 +0700

    update halaman home

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -71,7 +71,6 @@
         <div class="box">
           <img src="{{ asset('website/img/values-1.png') }}" class="img-fluid" alt="">
           <h3>Informatika</h3>
-          <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
         </div>
       </div>
 
@@ -79,7 +78,6 @@
         <div class="box">
           <img src="{{ asset('website/img/values-2.png') }}" class="img-fluid" alt="">
           <h3>Sistem Informasi</h3>
-          <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
         </div>
       </div>
 
@@ -87,7 +85,6 @@
         <div class="box">
           <img src="{{ asset('website/img/values-3.png') }}" class="img-fluid" alt="">
           <h3>Sains Data</h3>
-          <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
         </div>
       </div>
 
@@ -154,8 +151,8 @@
   <div class="container" data-aos="fade-up">
 
     <header class="section-header">
-      <h2>Features</h2>
-      <p>Laboriosam et omnis fuga quis dolor direda fara</p>
+      <h2>Fitur</h2>
+      <p>Fitur Unggulan Kami</p>
     </header>
 
     <div class="row">
@@ -170,42 +167,28 @@
           <div class="col-md-6" data-aos="zoom-out" data-aos-delay="200">
             <div class="feature-box d-flex align-items-center">
               <i class="bi bi-check"></i>
-              <h3>Eos aspernatur rem</h3>
+              <h3>Paperless</h3>
             </div>
           </div>
 
           <div class="col-md-6" data-aos="zoom-out" data-aos-delay="300">
             <div class="feature-box d-flex align-items-center">
               <i class="bi bi-check"></i>
-              <h3>Facilis neque ipsa</h3>
+              <h3>Cepat</h3>
             </div>
           </div>
 
           <div class="col-md-6" data-aos="zoom-out" data-aos-delay="400">
             <div class="feature-box d-flex align-items-center">
               <i class="bi bi-check"></i>
-              <h3>Volup amet voluptas</h3>
+              <h3>Mudah Digunakan</h3>
             </div>
           </div>
 
           <div class="col-md-6" data-aos="zoom-out" data-aos-delay="500">
             <div class="feature-box d-flex align-items-center">
               <i class="bi bi-check"></i>
-              <h3>Rerum omnis sint</h3>
-            </div>
-          </div>
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="600">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Alias possimus</h3>
-            </div>
-          </div>
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="700">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Repellendus mollitia</h3>
+              <h3>Terintegrasi</h3>
             </div>
           </div>
 
@@ -213,74 +196,6 @@
       </div>
 
     </div> <!-- / row -->
-
-    <!-- Feature Icons -->
-    <div class="row feature-icons" data-aos="fade-up">
-      <h3>Ratione mollitia eos ab laudantium rerum beatae quo</h3>
-
-      <div class="row">
-
-        <div class="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
-          <img src="{{ asset('website/img/features-3.png') }}" class="img-fluid p-4" alt="">
-        </div>
-
-        <div class="col-xl-8 d-flex content">
-          <div class="row align-self-center gy-4">
-
-            <div class="col-md-6 icon-box" data-aos="fade-up">
-              <i class="ri-line-chart-line"></i>
-              <div>
-                <h4>Corporis voluptates sit</h4>
-                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
-              </div>
-            </div>
-
-            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-              <i class="ri-stack-line"></i>
-              <div>
-                <h4>Ullamco laboris nisi</h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-              </div>
-            </div>
-
-            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-              <i class="ri-brush-4-line"></i>
-              <div>
-                <h4>Labore consequatur</h4>
-                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-              </div>
-            </div>
-
-            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-              <i class="ri-magic-line"></i>
-              <div>
-                <h4>Beatae veritatis</h4>
-                <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
-              </div>
-            </div>
-
-            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-              <i class="ri-command-line"></i>
-              <div>
-                <h4>Molestiae dolor</h4>
-                <p>Et fuga et deserunt et enim. Dolorem architecto ratione tensa raptor marte</p>
-              </div>
-            </div>
-
-            <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-              <i class="ri-radar-line"></i>
-              <div>
-                <h4>Explicabo consectetur</h4>
-                <p>Est autem dicta beatae suscipit. Sint veritatis et sit quasi ab aut inventore</p>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
-
-    </div><!-- End Feature Icons -->
 
   </div>
 
@@ -301,8 +216,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
         <div class="service-box blue">
           <i class="ri-discuss-line icon"></i>
-          <h3>Nesciunt Mete</h3>
-          <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
+          <h3>Perkuliahan</h3>
+          <p>Informasi seputar </p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>
@@ -310,8 +225,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
         <div class="service-box orange">
           <i class="ri-discuss-line icon"></i>
-          <h3>Eosle Commodi</h3>
-          <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+          <h3>Surat Pengantar</h3>
+          <p>Layanan pengajuan dan monitoring berbagai macam surat pengantar.</p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>
@@ -319,8 +234,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
         <div class="service-box green">
           <i class="ri-discuss-line icon"></i>
-          <h3>Ledo Markt</h3>
-          <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+          <h3>Surat Keterangan</h3>
+          <p>Layanan pengajuan dan monitoring berbagai macam surat keterangan.</p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>
@@ -328,8 +243,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
         <div class="service-box red">
           <i class="ri-discuss-line icon"></i>
-          <h3>Asperiores Commodi</h3>
-          <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+          <h3>Surat Rekomendasi</h3>
+          <p>Layanan pengajuan dan monitoring berbagai macam surat rekomendasi.</p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>
@@ -337,8 +252,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
         <div class="service-box purple">
           <i class="ri-discuss-line icon"></i>
-          <h3>Velit Doloremque.</h3>
-          <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
+          <h3>Surat Lainnya</h3>
+          <p>Layanan pengajuan dan monitoring berbagai macam surat-surat lain yang dibutuhkan mahasiswa.</p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>
@@ -346,8 +261,8 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
         <div class="service-box pink">
           <i class="ri-discuss-line icon"></i>
-          <h3>Dolori Architecto</h3>
-          <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+          <h3>Yudisium</h3>
+          <p>Layanan sistem informasi untuk proses yudisium.</p>
           <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
         </div>
       </div>

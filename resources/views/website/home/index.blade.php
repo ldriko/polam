9d4f3921407a199cdbba65875a09ae99caf7c5bd
Anwarul Fattach <emosekolah@gmail.com>
2025-03-14 10:37:11 +0700

    hapus keunggulan kami

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -145,62 +145,6 @@
   </div>
 </section><!-- End Counts Section -->
 
-<!-- ======= Features Section ======= -->
-<section id="features" class="features">
-
-  <div class="container" data-aos="fade-up">
-
-    <header class="section-header">
-      <h2>Fitur</h2>
-      <p>Fitur Unggulan Kami</p>
-    </header>
-
-    <div class="row">
-
-      <div class="col-lg-6">
-        <img src="{{ asset('website/img/features.png') }}" class="img-fluid" alt="">
-      </div>
-
-      <div class="col-lg-6 mt-5 mt-lg-0 d-flex">
-        <div class="row align-self-center gy-4">
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="200">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Paperless</h3>
-            </div>
-          </div>
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="300">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Cepat</h3>
-            </div>
-          </div>
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="400">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Mudah Digunakan</h3>
-            </div>
-          </div>
-
-          <div class="col-md-6" data-aos="zoom-out" data-aos-delay="500">
-            <div class="feature-box d-flex align-items-center">
-              <i class="bi bi-check"></i>
-              <h3>Terintegrasi</h3>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-    </div> <!-- / row -->
-
-  </div>
-
-</section><!-- End Features Section -->
-
 <!-- ======= Services Section ======= -->
 <section id="services" class="services">
 

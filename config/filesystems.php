515d0096af5d3af0bf76ba909e75d1f74380e6e9
Anwarul Fattach <emosekolah@gmail.com>
2025-04-23 07:09:16 +0700

    update config filesystem hosting

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,8 +46,8 @@ return [
 
         'hosting' => [
             'driver' => 'local',
-            'root' => public_path('storage'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL'),
             'visibility' => 'public',
             'throw' => false,
         ],
